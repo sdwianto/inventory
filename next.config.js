62b153fa8d11a1f66825a20471de807415dcbc5a
sdwianto <sdwianto@gmail.com>
2026-07-24 13:00:42 +0700

    finish p4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const nextConfig = {
   output: 'standalone',
-  transpilePackages: ['@sdwianto/contracts', '@sdwianto/events', '@sdwianto/metrics', '@sdwianto/platform'],
+  transpilePackages: [
+    '@sdwianto/contracts',
+    '@sdwianto/events',
+    '@sdwianto/metrics',
+    '@sdwianto/platform',
+    '@sdwianto/integration',
+  ],
   images: {
     unoptimized: true,
     remotePatterns: [
